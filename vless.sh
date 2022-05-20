d189abeb208d77a082766f024e354d37e084a175
sbox9981 <92682541+sbox9981@users.noreply.github.com>
2022-05-20 14:20:47 +0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -5,7 +5,7 @@ wget -N https://raw.githubusercontent.com/Misaka-blog/AX/main/web
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="1eb6e917-774b-4a84-aff6-b058577c60a5"
+    id="b3c38e92-2445-4542-93e6-1fe2dd6089c4"
 fi
 
 cat <<EOF > ~/config.json
