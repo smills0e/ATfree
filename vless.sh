49421408243029ff183f066d9ccd3ad02831b172
smills0e <93284696+smills0e@users.noreply.github.com>
2022-05-20 09:18:35 +0200

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -f web config.json
-wget -N https://raw.githubusercontent.com/Misaka-blog/AX/main/web
+wget -N https://raw.githubusercontent.com/smills0e/ATfree/main/web
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="b3c38e92-2445-4542-93e6-1fe2dd6089c4"
+    id="cbcd715e-b724-4abf-87b0-99af89665684"
 fi
 
 cat <<EOF > ~/config.json
